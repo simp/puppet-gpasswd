simp/puppet-gpasswd

commit d49cf32db1050e42c775e85ac190ee7b35f750b0

Pulled in the changes that were made by SIMP
Pulled in the SIMP changes and tests.

.fixtures.yml
@@ -1,0 +1,4 @@
+---
+fixtures:
+  symlinks:
+    gpasswd: "#{source_dir}"

Gemfile
@@ -1,13 +1,53 @@
-source 'https://rubygems.org'
+# ------------------------------------------------------------------------------
+# Environment variables:
+#   SIMP_GEM_SERVERS | a space/comma delimited list of rubygem servers
+#   PUPPET_VERSION   | specifies the version of the puppet gem to load
+# ------------------------------------------------------------------------------
+# NOTE: SIMP Puppet rake tasks support ruby 2.0 and ruby 2.1
+# ------------------------------------------------------------------------------
+puppetversion = ENV.key?('PUPPET_VERSION') ? "#{ENV['PUPPET_VERSION']}" : '~>3'
+gem_sources   = ENV.key?('SIMP_GEM_SERVERS') ? ENV['SIMP_GEM_SERVERS'].split(/[, ]+/) : ['https://rubygems.org']
 
-group :development, :test do
-  gem 'puppetlabs_spec_helper', :require => false
-  gem 'rspec-system-puppet', '~>2.0'
-  gem 'puppet-lint'
+gem_sources.each { |gem_source| source gem_source }
+
+group :test do
+  gem "rake"
+  gem 'puppet', puppetversion
+  gem "rspec", '< 3.2.0'
+  gem "rspec-puppet"
+  gem "hiera-puppet-helper"
+  gem "puppetlabs_spec_helper"
+  gem "metadata-json-lint"
+  gem "simp-rspec-puppet-facts", "~> 1.3"
+
+
+  # simp-rake-helpers does not suport puppet 2.7.X
+  if "#{ENV['PUPPET_VERSION']}".scan(/\d+/).first != '2' &&
+      # simp-rake-helpers and ruby 1.8.7 bomb Travis tests
+      # TODO: fix upstream deps (parallel in simp-rake-helpers)
+      RUBY_VERSION.sub(/\.\d+$/,'') != '1.8'
+    gem 'simp-rake-helpers'
+  end
 end
 
-if puppetversion = ENV['PUPPET_GEM_VERSION']
-  gem 'puppet', puppetversion, :require => false
-else
-  gem 'puppet', :require => false
+group :development do
+  gem "travis"
+  gem "travis-lint"
+  gem "travish"
+  gem "puppet-blacksmith"
+  gem "guard-rake"
+  gem 'pry'
+  gem 'pry-doc'
+
+  # `listen` is a dependency of `guard`
+  # from `listen` 3.1+, `ruby_dep` requires Ruby version >= 2.2.3, ~> 2.2
+  gem 'listen', '~> 3.0.6'
 end
+
+group :system_tests do
+  gem 'beaker'
+  gem 'beaker-rspec'
+  gem 'simp-beaker-helpers', '>= 1.0.5'
+end
+
+gem 'simp-rake-helpers'

lib/puppet/provider/group/gpasswd.rb
@@ -12,7 +12,6 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
             :delmember => 'gpasswd'
 
   has_feature :manages_members unless %w{HP-UX Solaris}.include? Facter.value(:operatingsystem)
-  has_feature :libuser if Puppet.features.libuser?
 
   def addcmd
     # This pulls in the main group add command should the group need
@@ -45,11 +44,9 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
   end
 
   def members
-    getinfo(true) if @objectinfo.nil?
     retval = @objectinfo.mem
 
-    if @resource[:members] and
-       ( @resource[:attribute_membership] == :minimum ) and
+    if ( @resource[:attribute_membership] == :minimum ) and
        (@resource[:members] - @objectinfo.mem).empty?
     then
         retval = @resource[:members]
@@ -95,7 +92,7 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
   def mod_group(cmds)
     cmds.each do |run_cmd|
       begin
-        execute(run_cmd,:custom_environment => @custom_environment)
+        execute(run_cmd)
       rescue Puppet::ExecutionFailure => e
         if $?.exitstatus == 3 then
           Puppet.warning("Modifying #{@resource[:name]} => #{e}")

.gitignore
@@ -1,0 +1,15 @@
+.*.sw?
+.yardoc
+dist
+/pkg
+/spec/fixtures
+/spec/rp_env
+!/spec/hieradata/default.yaml
+!/spec/fixtures/site.pp
+/.rspec_system
+/.vagrant
+/.bundle
+/Gemfile.lock
+/vendor
+/junit
+/log

.puppet-lint.rc
@@ -1,0 +1,4 @@
+--log-format="%{path}:%{line}:%{check}:%{KIND}:%{message}"
+--relative
+--no-class_inherits_from_params_class-check
+--no-80chars-check

.rspec
@@ -1,0 +1,3 @@
+--format documentation
+--color
+--fail-fast

.travis.yml
@@ -1,0 +1,97 @@
+---
+language: ruby
+sudo: false
+cache: bundler
+before_script:
+  - bundle
+bundler_args: --without development system_tests
+before_install: rm Gemfile.lock || true
+script:
+  - bundle exec rake test
+notifications:
+  email: false
+rvm:
+  - 2.0.0
+  - 2.1.0
+  - 2.2.1
+  - 1.9.3
+env:
+  global:
+    - STRICT_VARIABLES=yes
+    - TRUSTED_NODE_DATA=yes
+  matrix:
+    - PUPPET_VERSION="~> 3.8.0"
+    - PUPPET_VERSION="~> 4.4.0"
+    - PUPPET_VERSION="~> 3.5.0"
+    - PUPPET_VERSION="~> 3.6.0"
+    - PUPPET_VERSION="~> 3.7.0"
+    - PUPPET_VERSION="~> 3.7.0" FUTURE_PARSER=yes
+    - PUPPET_VERSION="~> 3.8.0" FUTURE_PARSER=yes
+    - PUPPET_VERSION="~> 4.0.0"
+    - PUPPET_VERSION="~> 4.1.0"
+    - PUPPET_VERSION="~> 4.2.0"
+    - PUPPET_VERSION="~> 4.3.0"
+matrix:
+  fast_finish: true
+  allow_failures:
+    - rvm: 1.9.3
+    - rvm: 2.2.1
+    - env: PUPPET_VERSION="~> 3.5.0"
+    - env: PUPPET_VERSION="~> 3.6.0"
+    - env: PUPPET_VERSION="~> 3.7.0"
+    - env: PUPPET_VERSION="~> 3.7.0" FUTURE_PARSER=yes
+    - env: PUPPET_VERSION="~> 3.8.0" FUTURE_PARSER=yes
+    - env: PUPPET_VERSION="~> 4.0.0"
+    - env: PUPPET_VERSION="~> 4.1.0"
+    - env: PUPPET_VERSION="~> 4.2.0"
+    - env: PUPPET_VERSION="~> 4.3.0"
+
+
+  exclude:
+  # Ruby 1.9.3
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 3.5.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 3.6.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 3.7.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 3.7.0" FUTURE_PARSER=yes
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 4.0.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 4.1.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 4.2.0"
+  - rvm: 1.9.3
+    env: PUPPET_VERSION="~> 4.3.0"
+
+  # Ruby 2.1.0
+  - rvm: 2.1.0
+    env: PUPPET_VERSION="~> 3.5.0"
+  - rvm: 2.1.0
+    env: PUPPET_VERSION="~> 3.6.0"
+  - rvm: 2.1.0
+    env: PUPPET_VERSION="~> 3.7.0"
+  - rvm: 2.1.0
+    env: PUPPET_VERSION="~> 3.7.0" FUTURE_PARSER=yes
+
+  # Ruby 2.2.1
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 3.5.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 3.6.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 3.7.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 3.7.0" FUTURE_PARSER=yes
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 3.8.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 4.0.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 4.1.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 4.2.0"
+  - rvm: 2.2.1
+    env: PUPPET_VERSION="~> 4.3.0"

Rakefile
@@ -1,26 +1,4 @@
-require 'rubygems'
-require 'bundler/setup'
+require 'simp/rake/pupmod/helpers'
 
-Bundler.require :default
+Simp::Rake::Pupmod::Helpers.new(File.dirname(__FILE__))
 
-require 'rspec/core/rake_task'
-require 'puppetlabs_spec_helper/rake_tasks'
-require 'rspec-system/rake_task'
-
-require 'puppet-lint/tasks/puppet-lint'
-PuppetLint.configuration.ignore_paths = ['vendor/**/*.pp']
-
-task :default do
-  Rake::Task.tasks.each do |task|
-    if task.comment then
-      puts "\n  #{task.to_s}"
-      puts "    - #{task.comment}"
-    end
-  end
-end
-
-desc 'Run reasonably quick tests for CI'
-task :ci => [
-  :lint,
-  :spec,
-]

commit af7354a0c522d3582aecb5b5ae0895f369f8ef8f

Multiple bug fixes
* Fixed README typos
* Cleaned up code
* Added acceptance tests
* Added support for `auth_membership`
* Fixed code regression from d49cf32
  * Thanks for the notification from John Warburton
* Validated noop message was correct
* Fixed sort order when checking user lists against the system

Closes #3
Closes #4

.fixtures.yml
@@ -1,4 +1,6 @@
 ---
 fixtures:
+  repositories:
+    stdlib: http://github.com/puppetlabs/puppetlabs-stdlib
   symlinks:
     gpasswd: "#{source_dir}"

Gemfile
@@ -35,8 +35,6 @@ group :development do
 end
 
 group :system_tests do
-  # This patch is required to fix Beaker's broken `aio` handling
-  gem 'beaker', :git => 'https://github.com/trevor-vaughan/beaker.git', :branch => 'BKR-978-2.51.0'
   gem 'beaker-rspec'
   gem 'simp-beaker-helpers', ENV.fetch('SIMP_BEAKER_HELPERS_VERSION', '~> 1.5')
 end

lib/puppet/provider/group/gpasswd.rb
@@ -12,16 +12,19 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
             :delmember => 'gpasswd'
 
   has_feature :manages_members unless %w{HP-UX Solaris}.include? Facter.value(:operatingsystem)
+  has_feature :libuser if Puppet.features.libuser?
 
   def addcmd
     # This pulls in the main group add command should the group need
     # to be added from scratch.
     cmd = Array(super.map{|x| x = "#{x}"}.shelljoin)
 
-    @resource[:members] and cmd += @resource[:members].map{ |x|
-      [ command(:addmember),'-a',x,@resource[:name] ].shelljoin
-    }
-    
+    if @resource[:members]
+      cmd += @resource[:members].map{ |x|
+        [ command(:addmember),'-a',x,@resource[:name] ].shelljoin
+      }
+    end
+
     mod_group(cmd)
 
     # We're returning /bin/true here since the Nameservice classes
@@ -35,7 +38,7 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
   def modifycmd(param, value)
     cmd = [command(param.to_s =~ /password_.+_age/ ? :password : :modify)]
     cmd << flag(param) << value
-    if @resource.allowdupe? and (param == :gid)
+    if @resource.allowdupe? && (param == :gid)
       cmd << "-o"
     end
     cmd << @resource[:name]
@@ -44,33 +47,35 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
   end
 
   def members
+    getinfo(true) if @objectinfo.nil?
     retval = @objectinfo.mem
 
-    if ( @resource[:attribute_membership] == :minimum ) and
-       (@resource[:members] - @objectinfo.mem).empty?
-    then
-        retval = @resource[:members]
+    if !@resource[:auth_membership] && (@resource[:members] - @objectinfo.mem).empty?
+      retval = @resource[:members]
     end
 
-    retval
+    retval.sort
+  end
+
+  def members_insync?(is, should)
+    Array(is).uniq.sort == Array(should).uniq.sort
   end
 
   def members=(members)
     cmd = []
     to_be_added = members.dup
-    if @resource[:attribute_membership] == :minimum
-      to_be_added = to_be_added | @objectinfo.mem
-    else
+    if @resource[:auth_membership]
       to_be_removed = @objectinfo.mem - to_be_added
       to_be_added = to_be_added - @objectinfo.mem
 
-      not to_be_removed.empty? and cmd += to_be_removed.map { |x|
+      !to_be_removed.empty? && cmd += to_be_removed.map { |x|
         [ command(:addmember),'-d',x,@resource[:name] ].shelljoin
       }
-
+    else
+      to_be_added = to_be_added | @objectinfo.mem
     end
 
-    not to_be_added.empty? and cmd += to_be_added.map { |x|
+    !to_be_added.empty? && cmd += to_be_added.map { |x|
       [ command(:addmember),'-a',x,@resource[:name] ].shelljoin
     }
 
@@ -92,7 +97,7 @@ Puppet::Type.type(:group).provide :gpasswd, :parent => Puppet::Type::Group::Prov
   def mod_group(cmds)
     cmds.each do |run_cmd|
       begin
-        execute(run_cmd)
+        execute(run_cmd, :custom_environment => @custom_environment)
       rescue Puppet::ExecutionFailure => e
         if $?.exitstatus == 3 then
           Puppet.warning("Modifying #{@resource[:name]} => #{e}")

spec/acceptance/nodesets/default.yml
@@ -1,0 +1,32 @@
+HOSTS:
+  server:
+    roles:
+      - server
+      - default
+      - master
+    platform:   el-7-x86_64
+    box:        puppetlabs/centos-7.2-64-nocm
+    box_url:    https://vagrantcloud.com/puppetlabs/boxes/centos-7.2-64-nocm
+    hypervisor: vagrant
+  centos-client:
+    roles:
+      - agent
+      - client
+    platform:   el-6-x86_64
+    box:        puppetlabs/centos-6.6-64-nocm
+    box_url:    https://vagrantcloud.com/puppetlabs/boxes/centos-6.6-64-nocm
+    hypervisor: vagrant
+# Needs testing
+#  ubuntu-client:
+#    roles:
+#      - agent
+#      - client
+#    platform:   ubuntu-14.04-x86_64
+#    box:        puppetlabs/ubuntu-14.04-64-nocm
+#    box_url:    https://vagrantcloud.com/puppetlabs/boxes/ubuntu-14.04-64-nocm
+#    hypervisor: vagrant
+CONFIG:
+  log_level: verbose
+  type:      foss
+  vagrant_memsize: 256
+  ## vb_gui: true

spec/acceptance/suites/default/00_default_spec.rb
@@ -1,0 +1,111 @@
+require 'spec_helper_acceptance'
+
+test_name 'gpasswd extension'
+
+describe 'gpasswd' do
+  hoopy_froods = [
+    'marvin',
+    'arthur',
+    'ford',
+    'zaphod',
+    'trillian'
+  ]
+
+  meddling_kids = [
+    'fred',
+    'daphne',
+    'velma',
+    'shaggy',
+    'scooby'
+  ]
+
+  let(:manifest){<<-EOM
+      $users = ['#{users.join("','")}']
+      $users.each |$user| { user { $user: ensure => 'present' } }
+
+      group { 'test': members => $users, auth_membership => #{auth_membership} }
+    EOM
+  }
+
+  hosts.each do |host|
+    context 'with a sorted list of users' do
+      let(:users) { hoopy_froods.sort }
+      let(:auth_membership) { true }
+
+      # Using puppet_apply as a helper
+      it 'should work with no errors' do
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
+
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, {:catch_changes => true})
+      end
+
+      it 'should have populated the group' do
+        group_members = on(host, 'getent group test').output.strip.split(':').last.split(',')
+
+        expect(group_members - users).to be_empty
+      end
+    end
+
+    context 'with an unsorted list of users' do
+      let(:users) { hoopy_froods - [hoopy_froods.last] }
+      let(:auth_membership) { true }
+
+      # Using puppet_apply as a helper
+      it 'should work with no errors' do
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
+
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, {:catch_changes => true})
+      end
+
+      it 'should have populated the group' do
+        group_members = on(host, 'getent group test').output.strip.split(':').last.split(',')
+
+        expect(group_members - users).to be_empty
+      end
+    end
+
+    context 'when replacing existing users' do
+      let(:users) { meddling_kids }
+      let(:auth_membership) { true }
+
+      # Using puppet_apply as a helper
+      it 'should work with no errors' do
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
+
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, {:catch_changes => true})
+      end
+
+      it 'should have populated the group' do
+        group_members = on(host, 'getent group test').output.strip.split(':').last.split(',')
+
+        expect(group_members - users).to be_empty
+      end
+    end
+
+    context 'when adding all users' do
+      let(:users) { hoopy_froods }
+      let(:auth_membership) { false }
+
+      # Using puppet_apply as a helper
+      it 'should work with no errors' do
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
+
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, {:catch_changes => true})
+      end
+
+      it 'should have populated the group' do
+        group_members = on(host, 'getent group test').output.strip.split(':').last.split(',')
+
+        expect(group_members - (users + meddling_kids)).to be_empty
+      end
+    end
+  end
+end

spec/acceptance/suites/default/nodesets
@@ -1,0 +1,1 @@
+../../nodesets

spec/unit/provider/group/gpasswd_spec.rb
@@ -24,7 +24,7 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
         :custom_environment => {},
         :failonfail => true,
         :combine => true)
-      provider.expects(:execute).with('/usr/sbin/groupadd -g 555 -o mygroup')
+      provider.expects(:execute).with('/usr/sbin/groupadd -g 555 -o mygroup', {:custom_environment => {}})
       provider.create
     end
 
@@ -35,7 +35,7 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
           :custom_environment => {},
           :failonfail => true,
           :combine => true)
-        provider.expects(:execute).with('/usr/sbin/groupadd -r mygroup')
+        provider.expects(:execute).with('/usr/sbin/groupadd -r mygroup', {:custom_environment => {}})
         provider.create
       end
     end
@@ -47,7 +47,7 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
           :custom_environment => {},
           :failonfail => true,
           :combine => true)
-        provider.expects(:execute).with('/usr/sbin/groupadd mygroup')
+        provider.expects(:execute).with('/usr/sbin/groupadd mygroup', {:custom_environment => {}})
         provider.create
       end
     end
@@ -58,10 +58,10 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
           :custom_environment => {},
           :failonfail => true,
           :combine => true)
-        provider.expects(:execute).with('/usr/sbin/groupadd mygroup')
+        provider.expects(:execute).with('/usr/sbin/groupadd mygroup', {:custom_environment => {}})
         resource[:members] = ['test_one','test_two','test_three']
         resource[:members].each do |member|
-          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup")
+          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup", {:custom_environment => {}})
         end
         provider.create
       end
@@ -72,10 +72,10 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
         Etc.stubs(:getgrnam).with('mygroup').returns(
           Struct::Group.new('mygroup','x','99999',[])
         )
-        resource[:attribute_membership] = 'minimum'
         resource[:members] = ['test_one','test_two','test_three']
+        resource[:auth_membership] = :false
         resource[:members].each do |member|
-          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup")
+          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup", {:custom_environment => {}})
         end
         provider.create
         provider.members=(resource[:members])
@@ -88,10 +88,10 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
         Etc.stubs(:getgrnam).with('mygroup').returns(
           Struct::Group.new('mygroup','x','99999',old_members)
         )
-        resource[:attribute_membership] = 'minimum'
+        resource[:auth_membership] = :false
         resource[:members] = ['test_one','test_two','test_three']
         (resource[:members] | old_members).each do |member|
-          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup")
+          provider.expects(:execute).with("/usr/bin/gpasswd -a #{member} mygroup", {:custom_environment => {}})
         end
         provider.create
         provider.members=(resource[:members])
@@ -104,14 +104,14 @@ describe Puppet::Type.type(:group).provider(:gpasswd) do
         Etc.stubs(:getgrnam).with('mygroup').returns(
           Struct::Group.new('mygroup','x','99999',old_members)
         )
-        resource[:attribute_membership] = 'inclusive'
+        resource[:auth_membership] = :true
         resource[:members] = ['test_one','test_two','test_three']
 
         (resource[:members] - old_members).each do |to_add|
-          provider.expects(:execute).with("/usr/bin/gpasswd -a #{to_add} mygroup")
+          provider.expects(:execute).with("/usr/bin/gpasswd -a #{to_add} mygroup", {:custom_environment => {}})
         end
         (old_members - resource[:members]).each do |to_del|
-          provider.expects(:execute).with("/usr/bin/gpasswd -d #{to_del} mygroup")
+          provider.expects(:execute).with("/usr/bin/gpasswd -d #{to_del} mygroup", {:custom_environment => {}})
         end
         provider.create
         provider.members=(resource[:members])